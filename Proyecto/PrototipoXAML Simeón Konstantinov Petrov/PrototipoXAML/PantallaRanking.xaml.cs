a75d192d52a4a7bee1ff4e3edbbae3d6fab240d6
simekons <simekons@ucm.es>
2021-05-11 14:34:08 +0200

    Prototipo XAML Simeón Konstantinov Petrov

---
+++ Proyecto/PrototipoXAML Simeón Konstantinov Petrov/PrototipoXAML/PantallaRanking.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PrototipoXAML
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class PantallaRanking : Page
+    {
+        public ObservableCollection<ViewModelRanking> ListaRanking { get; } = new ObservableCollection<ViewModelRanking>();
+        public PantallaRanking()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Atras_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(PantallaPrincipal));
+        }
+                
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Cosntruye las listas de ModelView a partir de la lista Modelo 
+            if (ListaRanking != null)
+                foreach (Ranking rango in ModelRanking.getAllRankings())
+                {
+                    ViewModelRanking VMitem = new ViewModelRanking(rango);
+                    ListaRanking.Add(VMitem);
+                }
+            base.OnNavigatedTo(e);
+        }
+    }
+}
---
+++ Proyecto/PrototipoXAML Simeón Konstantinov Petrov/PrototipoXAML/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace PrototipoXAML
+{
+    public class ViewModel : Logro
+    {
+        public Image Img;
+        public Image Img2;
+        public ContentControl CCImg;
+        public ContentControl CCImg2;
+        public ViewModel(Logro dron)
+        {
+            Id = dron.Id;
+            Nombre = dron.Nombre;
+            Imagen = dron.Imagen;
+            Descripcion = dron.Descripcion;
+            Recompensa = dron.Recompensa;
+            Coin = "Assets\\coins.png";
+            Img = new Image();
+            string s = System.IO.Directory.GetCurrentDirectory() + "\\" + dron.Imagen;
+            Img.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(s));
+            Img.Width = 50;
+            Img.Height = 50;
+            Img2 = new Image();
+            string s2 = System.IO.Directory.GetCurrentDirectory() + "\\" + Coin;
+            Img2.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(s));
+            Img2.Width = 50;
+            Img2.Height = 50;
+            CCImg = new ContentControl();
+            CCImg.Content = Img;
+            CCImg2 = new ContentControl();
+            CCImg2.Content = Img2;
+        }
+    }
+}
