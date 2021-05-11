c8c27edca5ba407efdd5724dbc99aadc9cbe275e
Adriamig <62662115+Adriamig@users.noreply.github.com>
2021-05-11 02:21:06 +0200

    Prototipo Adrián Migueles DAmbrosio

--- Proyecto/DSI_PROYECTO/DSI_PROYECTO/App.xaml.cs
+++ Proyecto/DSI_PROYECTO/DSI_PROYECTO/App.xaml.cs
@@ -66,7 +66,7 @@ namespace DSI_PROYECTO
                     // Cuando no se restaura la pila de navegación, navegar a la primera página,
                     // configurando la nueva página pasándole la información requerida como
                     //parámetro de navegación
-                    rootFrame.Navigate(typeof(ColeccionCartas), e.Arguments);
+                    rootFrame.Navigate(typeof(Tienda), e.Arguments);
                 }
                 // Asegurarse de que la ventana actual está activa.
                 Window.Current.Activate();
